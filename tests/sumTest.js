8928cfd1e4b6cdf7fcc6166f3fce1ff2b47ee19c
Igo Lapa <igo@gabstr.com>
2015-11-09 22:16:21 +0000

    trying to sum 10+10, but it fails 'cause the module sum it's only returning 2.

--- tests/sumTest.js
+++ tests/sumTest.js
@@ -3,7 +3,7 @@ var sum = require('../sum');
 
 // Let's create the test scope with describe
 describe('sum.js', function() {
-    it('sum of 1 + 1 should return 2', function() {
-        sum(1,1).should.be.equal(2);
+    it('sum of 10 + 10 should return 20', function() {
+        sum(10,10).should.be.equal(20);
     });
 });
