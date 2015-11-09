8472e8f6013907224c991069049dc0f7003aa6aa
Igo Lapa <igo@gabstr.com>
2015-11-09 22:39:48 +0000

    adding old 1+1 sum test, so the developer can see the first test working and the second failing.

--- tests/sumTest.js
+++ tests/sumTest.js
@@ -3,6 +3,9 @@ var sum = require('../sum');
 
 // 1st step: Let's create the test scope with describe
 describe('sum.js', function() {
+    it('sum of 1 + 1 should return 2', function() {
+        sum(1,1).should.be.equal(2);
+    });
     // Let's say that we want so test the sum of 10 + 10
     // Probably when you ran the code it has fail.
     it('sum of 10 + 10 should return 20', function() {
